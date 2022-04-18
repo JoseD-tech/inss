f3129b004174211e11236db58f2e50667e49ff03
Arnaldo Perez <arnaldoeperezb@gmail.com>
2022-04-18 12:52:50 -0400

    Banner store

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center" />
+  <v-row justify="center" align="center" dense>
+    <v-col cols="12" no-gutter>
+      <LandingBaner class="landing-banner" />
     </v-col>
   </v-row>
 </template>
 
 <script>
+import LandingBaner from '@/components/LandingBanner'
 import landingStore from '@/store/landing'
-
 export default {
   name: 'IndexPage',
+  components: { LandingBaner },
   setup () {
     const landing = landingStore()
     landing.inicializar()
